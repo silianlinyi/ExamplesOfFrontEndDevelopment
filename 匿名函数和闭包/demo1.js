40c2b80dfad8fc38639d78a3f5af26497f818155
silianlinyi <244098979@qq.com>
2014-10-11 22:29:32 +0800

    新增实例
    匿名函数与闭包

---
+++ 匿名函数和闭包/demo1.js
@@ -0,0 +1,12 @@
+(function() { /* code */ }());
+// 或者
+(function() { /* code */ })();
+
+var i = function() {return 10;}();
+true && function() { /* code */ }();
+0, function() { /* code */ }();
+
+!function() { /* code */ }();
+~function() { /* code */ }();
+-function() { /* code */ }();
++function() { /* code */ }();
